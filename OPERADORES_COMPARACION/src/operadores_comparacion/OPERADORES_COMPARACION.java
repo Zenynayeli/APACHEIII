3baf46aa18ab9acb1a060e570534ac9edfe0b7a6
Zenynayeli <1536527@senati.pe>
2024-04-25 07:16:58 -0500

    Mi tercer commit

---
+++ OPERADORES_COMPARACION/src/operadores_comparacion/OPERADORES_COMPARACION.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadores_comparacion;
+
+public class OPERADORES_COMPARACION {
+    public static void main(String[] args) {
+       int variableUno = 70;
+       int variableDos = 83; 
+        //Operador igual(==)
+       boolean igual = variableUno == variableDos;
+        System.out.println("Igual: " +igual);
+        
+          //Operador no igual(!=)
+       boolean noIgual = variableUno != variableDos;
+        System.out.println("No igual: " + noIgual);
+        
+          //Operador mayor que (>)
+       boolean mayorQue = variableUno > variableDos;
+        System.out.println("Mayo que: " + mayorQue);
+        
+        //Operador menor que (<)
+       boolean menorQue = variableUno < variableDos;
+        System.out.println("Menor que: " + menorQue);
+        
+          //Operador mayor o igual que (>=)
+       boolean mayorOIgualQue = variableUno >= variableDos;
+        System.out.println("mayorOIgualQue: " + mayorOIgualQue);
+        
+         //Operador menor o igual que (<=)
+       boolean menorOIgualQue = variableUno <= variableDos;
+        System.out.println("menorOIgualQue: " + menorOIgualQue);
+
+        
+        
+    }
+    
+}
